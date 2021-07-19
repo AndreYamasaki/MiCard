5fa55e3cc56a57607c057d0f7114f19f61cbea0e
Andre <andreyamasaki@brq.com>
2021-07-19 16:08:33 -0300

    Initial Commit

---
+++ MeeCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeeCard
+//
+//  Created by André Yamasaki on 19/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
